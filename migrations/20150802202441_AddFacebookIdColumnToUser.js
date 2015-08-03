2b3e8a3811d89d587e3973626ac37d781961fcd8
Arieh Smith <arieh.smith@gmail.com>
2015-08-02 20:41:27 -0400

    Add FB ID to users table

---
+++ migrations/20150802202441_AddFacebookIdColumnToUser.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.string("facebook_id");
+    table.index("facebook_id");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.dropColumn("facebook_id");
+  });
+};
