658c7a893d992ca9a4f4e7b1b35b72bf9dbb824b
Arieh Smith <arieh.smith@gmail.com>
2015-08-15 09:59:51 -0400

    Clean up FB auth callbacks and serialization

--- config/oauth.js
+++ config/oauth.js
@@ -3,10 +3,12 @@ var User = require('../models').User;
 module.exports = {
   facebook: {
     serializeUser: function(user, done) {
+      console.log("serializing user", user.get("id"));
       done(user.get("id"));
     },
 
     deserializeUser: function(id, done) {
+      console.log("deserializing user", id);
       new User({id: id})
         .fetch()
         .then(function(user) {
@@ -23,10 +25,16 @@ module.exports = {
 
     callback: function(accessToken, refreshToken, profile, done) {
       var userToSaveOrUpdate = new User({facebook_id: profile.id});
-      new User({facebook_id: profile.id})
+      userToSaveOrUpdate
         .fetch()
         .then(function(user) {
-          return user === undefined ? userToSaveOrUpdate.save().then(done) : done(user);
+          if (user === null) {
+            userToSaveOrUpdate.save().then(function(user, err) {
+              return done(err, user);
+            });
+          } else {
+            return done(null, user);
+          }
         });
     }
   }
